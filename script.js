69c0b850bffeef7c809d44559a5ed694dcede3fb
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-18 16:01:49 +0530

    Create a flexible container for the main content

---
+++ script.js
@@ -0,0 +1,25 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  /* Commit: Set global styling for box model */
+  body {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background-color: #0a0a23;
+    color: #ffffff;
+  }
+  
+  /* Commit: Set body styles and color scheme */
+  
+  .container {
+    width: 100%;
+    min-height: 100vh;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  
